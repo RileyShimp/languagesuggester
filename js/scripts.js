98dee6c06025ce58f59b0042aa31f9de227df760
Riley Shimp <rilryshimp@gmail.com>
2022-01-14 09:56:41 -0800

    WIP: Write JS code to show and hide results

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 
 
+function addTo(input) {
+  if (input === 0){
+    $("#ruby").show();
+  }
+  else if (input > 0 && input < 200 ){
+    $("#javascript").show();
+  }
+  else if (input > 200 && input < 2000){
+    $("#python").show();
+  }
+  else if (input > 2000){
+    $("#cSharp").show();
+  }
+
+}
+
+
 $(document).ready(function() {
-  $("form").submit(fuction(event)){
+  $("form").submit(function(event){
     event.preventDefault();
-    AudioDestinationNode()
+    addTo(parseInt($("#music").val()));
+    addTo(parseInt($("#food").val()));
+    addTo(parseInt($("#princess").val()));
   
   });
 });
