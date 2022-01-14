30d40d92ded60d8de5ff65a5c7eb15dac63269d1
Riley Shimp <rilryshimp@gmail.com>
2022-01-14 12:36:28 -0800

    Write js code to display name in results message

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
+    const name = $("input#name").val();
+
+    $(".nameresult").text(name);
+
     addTo((parseInt($("#music").val()))+(parseInt($("#food").val()))+(parseInt($("#princess").val()))+(parseInt($("#pets").val()))+(parseInt($("#giveUp").val())));
   });
 });
 
+
+
 function addTo(input) {
+  $("#message").hide();
   $("#ruby").hide();
   $("#javascript").hide();
   $("#python").hide();
   $("#cSharp").hide();
   if (input === 0){
     $("#ruby").show();
+    $("#message").show();
   }
   else if (input > 0 && input < 200){
     $("#javascript").show();
+    $("#message").show();
   }
   else if (input > 200 && input < 2000){
     $("#python").show();
+    $("#message").show();
   }
   else {
     $("#cSharp").show();
+    $("#message").show();
   }
 }
