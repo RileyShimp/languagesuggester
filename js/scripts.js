c42603331827e432ecaa19199350f75dd5c02978
Riley Shimp <rilryshimp@gmail.com>
2022-01-14 11:13:04 -0800

    Rewrite JS code to diplay result based on sum of values

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    addTo((parseInt($("#music").val()))+(parseInt($("#food").val()))+(parseInt($("#princess").val()))+(parseInt($("#pets").val()))+(parseInt($("#giveUp").val())));
+
+  });
+});
+
 
 function addTo(input) {
   $("#ruby").hide();
   $("#javascript").hide();
   $("#python").hide();
+  $("#cSharp").hide();
   if (input === 0){
     $("#ruby").show();
   }
-  else if (input > 0 && input < 200 ){
+  else if (input > 0 && input < 200){
     $("#javascript").show();
   }
-  else if (input > 200 ){
+  else if (input > 200 && input < 2000){
     $("#python").show();
   }
+  else {
+    $("#cSharp").show();
+  }
 }
-
-
-$(document).ready(function() {
-  $("form").submit(function(event){
-    event.preventDefault();
-    addTo(parseInt($("#music").val()));
-    addTo(parseInt($("#food").val()));
-    addTo(parseInt($("#princess").val()));
-  
-  });
-});
